51d8249b7f2c5b5676ced9e5775741134d8d4b09
rahul <shahrahul795@gmail.com>
2020-07-21 13:27:29 +0530

    before seprate meal card

--- appflix/lib/category_movies_screen.dart
+++ appflix/lib/category_movies_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "./dummy_data.dart";
 
 class CategoryMoviesScreen extends StatelessWidget {
   static const routeName = "category-movies";
@@ -9,13 +10,21 @@ class CategoryMoviesScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
 
     final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(categoryTitle),
-      ),
-      body: ListView.builder(itemBuilder: (ctx, index){
-
-      }, itemCount: ,)
-    );
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Text(
+                categoryMeals[index].title,
+                style: Theme.of(context).textTheme.bodyText1,
+              );
+            },
+            itemCount: categoryMeals.length));
   }
 }
