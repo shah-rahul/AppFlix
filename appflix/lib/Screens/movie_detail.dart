d6b6110e48cb753d666275d71b42627529bfcaa0
rahul <shahrahul795@gmail.com>
2020-07-21 19:58:45 +0530

    final build

--- appflix/lib/Screens/movie_detail.dart
+++ appflix/lib/Screens/movie_detail.dart
@@ -15,31 +15,45 @@ class MovieDetailScreeen extends StatelessWidget {
           child: Column(
         children: <Widget>[
           Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              selectedMeal.imageUrl,
-              fit: BoxFit.cover,
+            margin: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           Container(
             margin: EdgeInsets.symmetric(vertical: 10),
             child: Text(
               'Ingredients',
-              style: Theme.of(context).textTheme.headline6,
+              style: TextStyle(
+                color: Colors.white70,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
             ),
           ),
           Container(
+            // margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
-              color: Colors.white10,
-              border: Border.all(color: Colors.grey),
-            ),
-            height: 200,
-            width: 300,
+                color: Colors.white10,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
+            height: MediaQuery.of(context).size.height * 0.25,
+            width: MediaQuery.of(context).size.width * 0.7,
             child: ListView.builder(
                 itemBuilder: (ctx, index) => Card(
                       color: Colors.white54,
-                      child: Text(selectedMeal.ingredients[index]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Text(selectedMeal.ingredients[index]),
+                      ),
                     ),
                 itemCount: selectedMeal.ingredients.length),
           )
